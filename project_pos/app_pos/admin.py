fc4ee6d02601be24a84085fe08d39fac437f1042
sixdgt <c4crypt@gmail.com>
2025-12-16 09:45:23 +0545

    udpate: admin panel

--- project_pos/app_pos/admin.py
+++ project_pos/app_pos/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 from app_pos.models import Category, Product, Customer, Order, OrderItem
 # Register your models here.
 class AdminCategory(admin.ModelAdmin):
-    list_display = ('category_name', 'is_active', 'created_at')
-    search_fields = ('category_name',)
+    list_display = ('category_name', 'is_active', 'created_at') # columns
+    search_fields = ('category_name',) # search box
+    list_filter = ('is_active',) # filter sidebar
 
 class AdminProduct(admin.ModelAdmin):
     list_display = ('product_title', 'product_category', 'product_price', 'product_quantity', 'is_active', 'created_at')
@@ -15,3 +16,8 @@ admin.site.register(Product, AdminProduct)
 admin.site.register(Customer)
 admin.site.register(Order)
 admin.site.register(OrderItem)
+
+# customizing admin site titles
+admin.site.site_header = "Point of Sale Admin" 
+admin.site.site_title = "POS Admin Portal"
+admin.site.index_title = "Best POS System of Nepal"
